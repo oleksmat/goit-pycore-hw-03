ac0302757d2dedee3546951d2d12a79fc7721a38
Oleksandr Matvieiev <me@oleksmat.com>
2025-03-10 20:33:50 +0200

    add comments to task3; simplify logic

--- src/task3.py
+++ src/task3.py
@@ -1,16 +1,29 @@
 import re
 
 def normalize_phone(phone_number: str) -> str:
-    phone_number = phone_number.strip()
+    """
+        Normalizes phone number string passed in the argument
+
+        Normalization operations:
+        - Remove any non-number values (except a leading `+` symbol)
+        - Add default country code (`+38`) if phone number length is 10
+    :param phone_number: phone number string
+    :return: normalized number
+    """
+
+    # we remove all symbols except digits and `+` symbol
+    phone_number = re.sub('(\\D|\\+)', '', phone_number)
+
+    # we assume that after that we are left with these options:
+    # +\\d{12}
+    # 380\\d{9}
+    # 0\\d{9}
+    # so we add required prefixes
 
     if phone_number.startswith('+'):
-        phone_number = phone_number[1:]
-    else:
-        pass
+        return phone_number
 
-    phone_number = re.sub('\D', '', phone_number)
+    if phone_number.startswith('380'):
+        return '+' + phone_number
 
-    if len(phone_number) == 10:
-        return '+38' + phone_number
-
-    return '+' + phone_number
+    return '+38' + phone_number
